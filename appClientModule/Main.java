bc0081c5e6ef176bea306daeeaf6dba720c608db
Petar Petrov <pppepito86@gmail.com>
2017-12-04 01:55:56 +0200

    remove timeout from main client

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -4,11 +4,8 @@ import org.pesho.judge.SandboxResult;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		
-		SandboxResult result = new SandboxExecutor().readOutput(true).command(args[0])
-                .timeout(Double.valueOf(args[1])).execute();
+		SandboxResult result = new SandboxExecutor().readOutput(true).command(args[0]).execute();
 		System.out.println(result.getResult());
-		System.out.println(result.getTime());
 	}
 
 }
