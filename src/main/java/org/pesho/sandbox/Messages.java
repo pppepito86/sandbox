90ee30b772f72b80711f333d6f89c5a1bd805a9b
Petar Petrov <pppepito86@gmail.com>
2021-04-06 22:09:18 +0300

    fix time

--- src/main/java/org/pesho/sandbox/Messages.java
+++ src/main/java/org/pesho/sandbox/Messages.java
@@ -2,8 +2,8 @@ package org.pesho.sandbox;
 
 public class Messages {
 
-	public static final String TIME_LIMIT_EXCEEDED = "Extra time limit exceeded";
-	public static final String EXTRA_TIME_LIMIT_EXCEEDED = "Time limit exceeded";
+	public static final String TIME_LIMIT_EXCEEDED = "Time limit exceeded";
+	public static final String EXTRA_TIME_LIMIT_EXCEEDED = "Extra time limit exceeded";
 	public static final String WALL_CLOCK_TIMEOUT = "Wall clock timeout";
 	
 	public static final String MEMORY_LIMIT_EXCEEDED = "Memory limit exceeded";
